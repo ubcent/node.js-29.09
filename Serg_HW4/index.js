e7097da770cfc97ecb560553eccf1bd2b2e97a3f
Serg Diezz <xdiezz@gmail.com>
2018-10-12 23:10:26 +0300

    fin hw4 parser

--- Serg_HW4/index.js
+++ Serg_HW4/index.js
@@ -1,36 +1,60 @@
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
 const request = require('request');
 
 const site = 'http://lenta.ru';
 const pattern = /<\/time>(?!<\/div>)(.*?)<\/a>/gm;
+
 const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
 
 app.set('view engine', 'pug');
 app.set('views', path.resolve(__dirname, 'views'));
 
-function renderIndex(site) {
-  request(site, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      parse(body, pattern);
-    }
-  });
-}
+app.get('/', (req, res) => {
+  res.render('index', {title: 'Новости'});
+});
 
-function parse(body, pattern) {
-  let matches;
-  const matchesArray = [];
+app.post('/news', async (req, res) => {
+  let counts = Number.parseInt(req.body.counts);
+  counts = Number.isInteger(counts) ? counts : 10;
+  counts = (counts < 1 || counts > 10) ? 10 : counts;
 
-  while ((matches = pattern.exec(body)) !== null) {
-    matchesArray.push(matches[1].replace(/&nbsp;/g, ' '));
+  const getNews = await parseNews(pattern);
+
+  if (getNews.length !== 0) {
+    res.render('news', {title: 'Новости', getNews, counts});
+  } else {
+    res.send('Новостей нет');
   }
+});
 
-  app.get('/', (req, res) => {
-    res.render('index', {title: 'Новости', matchesArray});
+function requestNews(site) {
+  return new Promise((resolve, reject) => {
+    request(site, (error, res, body) => {
+      if (!error && res.statusCode == 200) {
+        resolve(body);
+      } else {
+        reject(error);
+      }
+    });
   });
 }
 
-renderIndex(site);
+async function parseNews(pattern) {
+  let matches;
+  const newsArray = [];
+  const news = await requestNews(site);
+
+  while ((matches = pattern.exec(news)) !== null) {
+    newsArray.push(matches[1].replace(/&nbsp;/g, ' '));
+  }
+  return newsArray;
+}
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
