0491f3a725ebdb990e1689b990055d26fc78aac8
Mikhail <mephist0_87@mail.ru>
2018-10-17 10:04:59 +0000

    site parser

--- uvarov_mikhail/site_parse/index.js
+++ uvarov_mikhail/site_parse/index.js
@@ -3,7 +3,7 @@ const cheerio = require('cheerio');
 
 let time = Date.now(); 
 
-const url = 'https://vihr-motors.ru/catalog/19/filter/brand_custom-is-sea-..'; 
+const url = 'https://ilodki.ru/pvh_lodki-m1/fregat-d1/'; 
 
 request( 
   { 
@@ -18,28 +18,31 @@ request(
     console.log('Время загрузки страницы: ' + url + ' ' + time + ' ms'); 
 
     const $ = cheerio.load(body); 
-    const priceList = $('.startshop_col'); 
+    const priceList = $('.product'); 
 
     priceList.each((idx, item) => { 
 
       let product = cheerio.load(item); 
-      let name = product('.name').text().trim(); 
+      let name = product('.hat').text().trim();
+      
+      let realPrice = parsePrice(product('.realPrice').text().trim());
+      
+      let oldPrice = parsePrice(product('.oldPrice').text().trim()); 
+      
+     
+      
+      let priceString = 
+        (oldPrice > 0) ? 
+          ('старая цена: ' + oldPrice + ' р; цена со скидкой: ') : 
+          'цена: ';
+          
+      priceString += realPrice;
+      
+      console.log( 
+        (idx + 1) + '. ' + name + ' ' + 
+        priceString + ' р;'); 
 
-      let realPrice = parseFloat( 
-      del_spaces(product('.new_price').text().trim()) 
-    ); 
-
-    let oldPrice = product('.oldPrice').text().trim(); 
-    if (oldPrice.length > 0) { 
-      oldPrice = parseFloat(del_spaces(oldPrice)); 
-    } else { 
-      oldPrice = 0; 
-    } 
-
-    console.log( 
-      (idx + 1) + '. ' + name + ' ' + 
-      (oldPrice > 0) ? ('старая цена: ' + oldPrice + ' р; цена со скидкой: ') : '' + 
-      realPrice + ' р;') 
+    
   }); 
 
   } else { 
@@ -50,4 +53,16 @@ request(
 function del_spaces(str) { 
   str = str.replace(/\s/g, ''); 
   return str; 
+}
+
+function parsePrice(price) {
+	
+	price = price.toString().replace(/\руб./g, '');	
+	price = del_spaces(price);
+	
+	if (price.length == 0 || price == 0) {
+		return 0;
+	}
+	
+	return parseFloat(price);
 }
