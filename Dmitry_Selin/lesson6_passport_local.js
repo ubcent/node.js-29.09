4464f82e8de76e3bd7a4bb4ca017b085171fcfc6
dmitry-vs <dmitryselin@inbox.ru>
2018-10-23 12:20:15 +0300

    Updated passport local login

--- Dmitry_Selin/lesson6_passport_local.js
+++ Dmitry_Selin/lesson6_passport_local.js
@@ -7,38 +7,9 @@ const path = require('path');
 const mongoose = require('mongoose');
 const LocalStrategy = require('passport-local').Strategy;
 
-const Task = require('./models/task');
+const TodoList = require('./helpers/todolist')
 const User = require('./models/user');
 mongoose.connect('mongodb://localhost/todolist', { useNewUrlParser: true, useCreateIndex: true });
-
-class TodoList{
-  static async addTask(name){
-    await (new Task({name})).save();
-  }
-
-  static async deleteTask(id){
-    await Task.deleteOne({_id : id});
-  }
-  
-  static async markTaskDone(id){
-    await Task.updateOne(
-      {_id: id}, 
-      {$set: {done: true, updatedAt: new Date()}}
-    );
-  }
-
-  static async markTaskUndone(id){
-    await Task.updateOne(
-      {_id: id}, 
-      {$set: {done: false, updatedAt: new Date()}}
-    );
-  }
-
-  static async getTasks(){
-    const tasks = await Task.find();
-    return tasks;
-  }
-}
 
 const app = express();
 app.engine('hbs', consolidate.handlebars);
@@ -60,12 +31,13 @@ app.use(passport.session());
 passport.use('local', new LocalStrategy({
     usernameField: 'username',
     passwordField: 'password',
-  },
-  (username, password, done) => {
-    if(username !== 'admin'){
+  }, async (username, password, done) => {
+    const user = await User.findOne({name: username});
+    if(!user) {
       return done(null, false);
     }
-    if(password !== 'a'){
+    const correctPassword = await user.checkPassword(password);
+    if(!correctPassword) {
       return done(null, false);
     }
     return done(null, {username});
@@ -106,30 +78,27 @@ app.get('/todo', mustBeAuthenticated, async (req, res) => {
 });
 
 app.post('/todo', mustBeAuthenticated, async (req, res) => {
-  if(req.body.action === 'newTask'){
-    await TodoList.addTask(req.body.name);
-  } 
-  else if(req.body.action === 'deleteTask'){
-    await TodoList.deleteTask(req.body.id);
-  }
-  else if(req.body.action === 'taskDone'){
-    await TodoList.markTaskDone(req.body.id);
-  }
-  else if(req.body.action === 'taskUndone'){
-    await TodoList.markTaskUndone(req.body.id);
-  }
-  else if(req.body.action === 'logout'){
+  if(req.body.action === 'logout'){
     req.logout();
-    res.redirect('/');
-    return;
-  }
-
+    return res.redirect('/login');
+  } else if(req.body.action === 'newTask'){
+    await TodoList.addTask(req.body.name);
+  } else if(req.body.action === 'deleteTask'){
+    await TodoList.deleteTask(req.body.id);
+  } else if(req.body.action === 'taskDone'){
+    await TodoList.markTaskDone(req.body.id);
+  } else if(req.body.action === 'taskUndone'){
+    await TodoList.markTaskUndone(req.body.id);
+  } else {
+    return res.send('Error: incorrect request');
+  } 
+  
   const tasks = await TodoList.getTasks();
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(tasks));
+  res.send(tasks);
 });
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.status(404).send("404: Not found")
 });
 
