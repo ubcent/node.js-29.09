adceab6134bef1c0ce29f9e033d3f57bb0b2d251
viktor <v.kalyaev@gmail.com>
2018-10-24 11:17:56 +0300

    Lesson_7

---
+++ Viktor_Kalyaev/models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: { type: String },
+  password: { type: String },
+  fullName: { type: String },
+});
+
+module.exports = mongoose.model('User', userSchema);
